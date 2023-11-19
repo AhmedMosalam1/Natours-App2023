694b4c3a6676326339928ecd3e64b544be89494e
AhmedMosalam1 <Ahmed.mosalam161@gmail.com>
2023-11-19 19:35:15 +0200

    ...

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12658,7 +12658,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "14251" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4378" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
